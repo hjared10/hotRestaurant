2c957e93b715ed260b28b38312e452fe828ebdc4
Alex Mills <alex@kcbas.com>
2020-02-22 10:42:56 -0600

    added tables and reserve routes

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/reserve.html"));
+});
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
